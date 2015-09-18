c3d0cce531f1dc30c26766534bc394ca2d809415
zainee <zainee@192.168.1.2>
2015-09-18 06:32:18 -0700

    second comment

--- gitrepo/src/gitrepo/hellogit.java
+++ gitrepo/src/gitrepo/hellogit.java
@@ -2,7 +2,8 @@ package gitrepo;
 
 public class hellogit {
 	 public static void main(String[] args) { 
-	      System.out.println("Hello, World");
+	      System.out.println("Hello, git");
+	      System.out.println("this is my first comment to github");
 	   }
 
 }
