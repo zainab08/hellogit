d0ce41e2af6f339e0e8228299ec2fb18297805f4
zainee <zainee@192.168.1.2>
2015-09-18 06:41:54 -0700

    first commnt

---
+++ secondrepo/src/secondrepo/secondclass.java
@@ -0,0 +1,9 @@
+package secondrepo;
+
+public class secondclass {
+	 public static void main(String[] args) { 
+	      System.out.println("Hello, git");
+	      System.out.println("this is my secondrepo to github");
+	   }
+
+}
